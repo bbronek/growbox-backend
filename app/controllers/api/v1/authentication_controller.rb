7c3d13751217f857c9a2d2045561ff69cf5230aa
bbronek <barbro214@gmail.com>
2023-04-12 02:04:00 +0200

    Handle login and logout

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  module V1
+    class AuthenticationController < ApplicationController
+      def login
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+          jwt_token = user.generate_jwt
+          render json: { token: jwt_token }
+        else
+          render json: { error: 'Invalid email or password' }, status: :unauthorized
+        end
+      end
+
+      def logout
+        current_user.invalidate_token
+        head :ok
+      end
+
+      private
+
+      def current_user
+        @current_user ||= User.find_by(id: payload['user_id'])
+      end
+
+      def payload
+        @payload ||= JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.secret_key_base, true)[0]
+      end
+    end
+  end
+end
