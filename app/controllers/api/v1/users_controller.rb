32d2356ac2a437b27efd0a98dc47ba3c1a57396d
bbronek <barbro214@gmail.com>
2023-04-12 02:03:32 +0200

    Handle registration

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class UsersController < BaseController
-      before_action :authenticate_user!
+      before_action :authenticate_user!, only: [:index, :show]
 
       def index
         render json: User.all
@@ -12,6 +12,17 @@ module Api
         render json: user
       end
 
+      def create
+        user = User.new(user_params)
+
+        if user.save
+          jwt_token = user.generate_jwt
+          render json: { token: jwt_token }
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def authenticate_user!
@@ -19,8 +30,12 @@ module Api
         if jwt_token && verify_jwt(jwt_token)
           @current_user = User.find(verify_jwt(jwt_token)["user_id"])
         else
-          render json: {error: "Unauthorized"}, status: :unauthorized
+          render json: { error: "Unauthorized" }, status: :unauthorized
         end
+      end
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
       end
     end
   end
