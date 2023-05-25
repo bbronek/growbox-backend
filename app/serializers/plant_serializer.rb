5a91eecdd0992047ae25e7565b0be4c0b7ee7864
bbronek <barbro214@gmail.com>
2023-05-26 00:32:34 +0200

    Fix bug with soil_humidity_max There was a typo

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -5,7 +5,7 @@ class PlantSerializer < ActiveModel::Serializer
     :temp_min, :temp_max, :air_humidity_min, :air_humidity_max,
     :fertilizing, :repotting, :pruning, :common_diseases,
     :appearance, :blooming_time, :public, :created_at, :updated_at, :description, :soil_humidity_min,
-    :soil_humidity_min, :image_url
+    :soil_humidity_max, :image_url
 
   def image_url
     rails_blob_url(object.image) if object.image.attached?
