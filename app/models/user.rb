9959d3acf66a6f59203de1fa55353ad6bbf1a1f0
bbronek <barbro214@gmail.com>
2023-06-04 02:04:11 +0200

    Fix password regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   has_many :favorite_plants_list, through: :favorite_plants, source: :plant
 
   validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
-  validates :password, presence: true, length: {minimum: 8}, format: {with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}\z/, message: "must be at least 8 characters and include at least one letter and one number"}
+  validates :password, presence: true, length: {minimum: 8}, format: {with: /\A(?=.*[a-zA-Z])(?=.*\d).{8,}\z/, message: "must be at least 8 characters and include at least one letter and one number"}
+
+
 
   def generate_jwt
     JWT.encode({user_id: id, exp: Time.now.to_i + 1800}, Rails.application.secrets.secret_key_base)
