096c8fab038f0cda1eb475c994429c382d12c478
bbronek <barbro214@gmail.com>
2023-11-27 03:59:07 +0100

    Add device logs api docs

--- app/controllers/api/v1/device_logs_controller.rb
+++ app/controllers/api/v1/device_logs_controller.rb
@@ -4,11 +4,16 @@ module Api
       before_action :authenticate_user!
       before_action :set_device, only: [:index, :show]
 
+      api :GET, '/v1/device_logs', 'Get all device logs for a specific device'
+      param :device_id, Integer, desc: 'ID of the device for which logs are to be shown', required: true
       def index
         @logs = @device.device_logs
         render json: @logs
       end
 
+      api :GET, '/v1/device_logs/:id', 'Show details of a specific device log'
+      param :device_id, Integer, desc: 'ID of the device to which the log belongs', required: true
+      param :id, Integer, desc: 'ID of the log to be shown', required: true
       def show
         @log = @device.device_logs.find(params[:id])
         render json: @log
