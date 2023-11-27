69e041d475c986b36d6210c073115e04d82cfcb5
bbronek <barbro214@gmail.com>
2023-11-27 04:13:07 +0100

    Update apipie config

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,6 +2,6 @@ Apipie.configure do |config|
   config.app_name                = "GrowboxBackend"
   config.api_base_url            = "/api"
   config.doc_base_url            = "/apipie"
-  # where is your API defined?
+  config.app_info                = "GreenMind API description"
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
 end
