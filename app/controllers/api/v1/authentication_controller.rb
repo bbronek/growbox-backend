c348cb505ca2153da937973993f2610c9dc64106
bbronek <barbro214@gmail.com>
2023-11-27 03:59:17 +0100

    Add authentication controller api docs

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,16 +2,21 @@ module Api
   module V1
     class AuthenticationController < BaseController
       before_action :authenticate_user!, only: [:logout]
+
+      api :POST, '/v1/login', 'Authenticate and obtain a JWT token'
+      param :email, String, desc: 'User email', required: true
+      param :password, String, desc: 'User password', required: true
       def login
         user = User.find_by(email: user_params[:email])
         if user&.authenticate(user_params[:password])
           jwt_token = user.generate_jwt
-          render json: {token: jwt_token}
+          render json: { token: jwt_token }
         else
-          render json: {error: "Invalid email or password"}, status: :unauthorized
+          render json: { error: 'Invalid email or password' }, status: :unauthorized
         end
       end
 
+      api :POST, '/v1/logout', 'Invalidate the JWT token and log out the user'
       def logout
         @current_user.invalidate_token
         head :ok
