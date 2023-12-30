33f02952e4facdbb447f81bdb77be757abd25f88
bbronek <barbro214@gmail.com>
2023-12-30 19:29:59 +0100

    Add cascading removal on device's plants and device's device logs

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,6 @@
 class Device < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :device_logs
-  has_many :plants
+  has_many :device_logs, dependent: :destroy
+  has_many :plants, dependent: :destroy
   has_one_attached :image
 end
